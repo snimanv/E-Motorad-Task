b087bce2aebf7fe0807c4d652c25b4776c0a3e8b
Snigdha V <137260280+snimanv@users.noreply.github.com>
2024-11-07 11:36:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, OAuthProvider } from 'firebase/auth';
 
 // Replace these values with your Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQ95rLsOffNkASP0K4QGBh4umd5JkwPC8",
-  authDomain: "reactdashboardtest1.firebaseapp.com",
-  projectId: "reactdashboardtest1",
-  storageBucket: "reactdashboardtest1.firebasestorage.app",
-  messagingSenderId: "110324151913",
-  appId: "1:110324151913:web:f83d409f179971e013d4f8",
-  measurementId: "G-67FHFGSZ7D",
+  apiKey: "api_key",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
